bc10569b93d93ebcfe08c496f436c68ac8579fcb
Liang Ma <liangma@liangbit.com>
2018-03-26 22:10:37 +0100

    update the root node hash value

--- lesson_2/trie/exercises/ex2d.py
+++ lesson_2/trie/exercises/ex2d.py
@@ -3,7 +3,7 @@ sys.path.append('src')
 import trie, utils, rlp
 
 #initialize trie from previous hash; add new [key, value] where key has common prefix
-state = trie.Trie('triedb', '15da97c42b7ed2e1c0c8dab6a6d7e3d9dc0a75580bbc4f1f29c33996d1415dcc'.decode('hex'))
+state = trie.Trie('triedb', '4a5b19d151e796482b08a1e020f1f7ef5ea7240c0171fd629598fee612892a7b'.decode('hex'))
 print state.root_hash.encode('hex')
 print state.root_node
 print ''
